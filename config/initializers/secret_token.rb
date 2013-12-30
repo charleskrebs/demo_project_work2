d3a4091d388f8c9a345cef2e063145db60f7231d
Charles Krebs <charleskrebs@gmail.com>
2013-12-30 12:13:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoProjectWork2::Application.config.secret_key_base = 'e9ca9923cb2e35dc780dd26ebce35e9eb423bcdb3c4868674e18f297cc49ee6763f8b8edc538b2ea655ee261403bcef78529d0d6f624d908e004493eb5b19208'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoProjectWork2::Application.config.session_store :cookie_store, key: '_demo_project_work2_session'
